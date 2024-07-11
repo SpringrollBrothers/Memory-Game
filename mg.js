1a42d4b0d00bf6896df14e1fce478e9bec8c3e56
Lu Lu <lulu@Lu.local>
2024-07-11 17:41:51 +0200

    added new functions for week 2 memory game

--- mg.js
+++ mg.js
@@ -1,14 +1,30 @@
 const backImg = "img/back.jpeg";
-const frontImg = "img/front.jpg";
-let showFront = false;
+const frontImg = [
+  "img/front.jpg",
+  "img/front1.jpeg",
+  "img/front2.jpeg",
+  "img/front3.jpeg",
+  "img/front4.avif",
+  "img/front5.jpeg",
+];
+const doubledFrontImgs = [...frontImg, ...frontImg];
 
-function createCardButton() {
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+function createCardButton(frontImg) {
   const cardButton = document.createElement("button");
   cardButton.className = "card";
 
   const img = document.createElement("img");
   img.src = backImg;
   cardButton.appendChild(img);
+  let showFront = false;
 
   cardButton.addEventListener("click", () => {
     if (showFront) {
@@ -22,5 +38,8 @@ function createCardButton() {
 
   return cardButton;
 }
+const shuffledFrontImgs = shuffleArray(doubledFrontImgs);
 const gameBoard = document.querySelector(".game-board");
-gameBoard.appendChild(createCardButton());
+shuffledFrontImgs.forEach((frontImg) => {
+  gameBoard.appendChild(createCardButton(frontImg));
+});
