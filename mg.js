3124eb0c189644a51478f1f1aa1911cb23f7347a
Lu Lu <lulu@Lu.local>
2024-08-09 15:44:53 +0200

    moved "shuffleArray" and "createCardButton" out of then callback function

--- mg.js
+++ mg.js
@@ -1,3 +1,54 @@
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+const backImg = "img/back.jpeg";
+let flipCount = 0;
+let startTime = null;
+let timerInterval = null;
+let flippedCards = [];
+const flipBackDelay = 2000;
+
+function createCardButton(frontImg) {
+  const cardButton = document.createElement("button");
+  cardButton.className = "card";
+  const img = document.createElement("img");
+  img.src = backImg;
+  cardButton.appendChild(img);
+  let showFront = false;
+  cardButton.addEventListener("click", () => {
+    if (!startTime) {
+      startTime = Date.now();
+      timerInterval = setInterval(() => {
+        const elapsedTime = parseInt((Date.now() - startTime) / 1000);
+        console.log("Elapsed time: " + elapsedTime + " seconds");
+      }, 1000);
+    }
+    if (showFront) {
+      img.src = backImg;
+    } else {
+      img.src = frontImg;
+      flipCount++;
+      console.log("Total flips: " + flipCount);
+      flippedCards.push(cardButton);
+      if (flippedCards.length === 2) {
+        setTimeout(() => {
+          flippedCards.forEach((card) => {
+            const img = card.querySelector("img");
+            img.src = backImg;
+          });
+          flippedCards = [];
+        }, flipBackDelay);
+      }
+    }
+    showFront = !showFront;
+  });
+
+  return cardButton;
+}
 
 document.addEventListener("DOMContentLoaded", function () {
   let frontImg = [];
@@ -13,8 +64,6 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((data) => {
       frontImg = data;
 
-      const backImg = "img/back.jpeg";
-
       const doubledFrontImgs = [...frontImg, ...frontImg];
       const shuffledFrontImgs = shuffleArray(doubledFrontImgs);
       const gameBoard = document.querySelector(".game-board");
@@ -24,61 +73,6 @@ document.addEventListener("DOMContentLoaded", function () {
       shuffledFrontImgs.forEach((frontImg) => {
         cardContainer.appendChild(createCardButton(frontImg));
       });
-
-      let flipCount = 0;
-      let startTime = null;
-      let timerInterval = null;
-      let flippedCards = [];
-      const flipBackDelay = 2000;
-
-      function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-      }
-
-      function createCardButton(frontImg) {
-        const cardButton = document.createElement("button");
-        cardButton.className = "card";
-
-        const img = document.createElement("img");
-        img.src = backImg;
-        cardButton.appendChild(img);
-        let showFront = false;
-
-        cardButton.addEventListener("click", () => {
-          if (!startTime) {
-            startTime = Date.now();
-            timerInterval = setInterval(() => {
-              const elapsedTime = parseInt((Date.now() - startTime) / 1000);
-              console.log("Elapsed time: " + elapsedTime + " seconds");
-            }, 1000);
-          }
-          if (showFront) {
-            img.src = backImg;
-          } else {
-            img.src = frontImg;
-            flipCount++;
-            console.log("Total flips: " + flipCount);
-            flippedCards.push(cardButton);
-
-            if (flippedCards.length === 2) {
-              setTimeout(() => {
-                flippedCards.forEach((card) => {
-                  const img = card.querySelector("img");
-                  img.src = backImg;
-                });
-                flippedCards = [];
-              }, flipBackDelay);
-            }
-          }
-          showFront = !showFront;
-        });
-
-        return cardButton;
-      }
     })
     .catch((error) => console.error(error));
 });
